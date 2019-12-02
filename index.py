597472378761d1ae7c3cc9e9fb7230d4e017f3d0
wadasworths <longfellow.wang@gmail.com>
2019-12-02 16:45:39 +0800

    Add png & Fix Image Name Chinese Bug

--- index.py
+++ index.py
@@ -2,7 +2,9 @@ import os
 import h5py
 import numpy as np
 import argparse
-
+#import sys
+#reload(sys)
+#sys.setdefaultencoding("utf8")
 from extract_cnn_vgg16_keras import VGGNet
 
 
@@ -15,7 +17,7 @@ args = vars(ap.parse_args())
 
 # 需要处理的图像数据目录 
 def get_imlist(path):
-    return [os.path.join(path,f) for f in os.listdir(path) if f.endswith('.jpg')]
+    return [os.path.join(path,f) for f in os.listdir(path) if f.endswith('.jpg') or f.endswith('.png')]
 
 # 获取图像特征，生成h5索引文件
 if __name__ == "__main__":
@@ -45,8 +47,10 @@ if __name__ == "__main__":
     print("      writing feature extraction results ...")
     print("--------------------------------------------------")
 
-
     h5f = h5py.File(output, 'w')
     h5f.create_dataset('dataset_1', data = feats)
+
+    # 图片名中有特殊字符，中文等
+    names = [name.encode() for name in names]
     h5f.create_dataset('dataset_2', data = np.string_(names))
     h5f.close()
